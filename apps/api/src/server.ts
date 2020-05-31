9fe24dc7d3c21970b4d45673406806c5c70834d5
Italo Felipe <felipe.italo@hotmail.com>
2020-05-31 11:11:56 -0300

    ES Lint setup done, added placeholder logic

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,5 +1,40 @@
 import { GraphQLServer } from 'graphql-yoga'
+import { resolve } from 'path'
 
-const server = new GraphQLServer({})
+const typeDefs = resolve(__dirname, 'schema.graphql')
+const USERS = [
+  { id: 1, name: 'The Lich King', email: 'arthas@wow.com' },
+  { id: 2, name: 'Jaina Proudmoore', email: 'jaina@wow.com' },
+  { id: 3, name: 'Uther', email: 'uther@wow.com' }
+]
 
-export default server;
+const resolvers = {
+  User: {
+    name: (parent): string => {
+      console.log('Parent: ', parent)
+      return 'User: ' + parent.name
+    }
+  },
+  Query: {
+    users: (): typeof USERS => USERS
+  },
+  Mutation: {
+    createUser: (parent, args): typeof USERS[0] => {
+      const { data } = args
+      console.log('args: ', args)
+
+      const user = {
+        ...data,
+        id: USERS.length + 1
+      }
+      USERS.push(user)
+      return user
+    }
+  }
+}
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers
+})
+
+export default server
