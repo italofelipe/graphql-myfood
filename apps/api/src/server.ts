0c5d116634c5dd88f961964c9f81c956787af7f1
Italo Felipe <felipe.italo@hotmail.com>
2020-05-31 14:12:16 -0300

    Added prettierrc configs

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,40 +1,40 @@
-import { GraphQLServer } from 'graphql-yoga'
-import { resolve } from 'path'
+import { GraphQLServer } from 'graphql-yoga';
+import { resolve } from 'path';
 
-const typeDefs = resolve(__dirname, 'schema.graphql')
+const typeDefs = resolve(__dirname, 'schema.graphql');
 const USERS = [
   { id: 1, name: 'The Lich King', email: 'arthas@wow.com' },
   { id: 2, name: 'Jaina Proudmoore', email: 'jaina@wow.com' },
-  { id: 3, name: 'Uther', email: 'uther@wow.com' }
-]
+  { id: 3, name: 'Uther', email: 'uther@wow.com' },
+];
 
 const resolvers = {
   User: {
     name: (parent): string => {
-      console.log('Parent: ', parent)
-      return 'User: ' + parent.name
-    }
+      console.log('Parent: ', parent);
+      return 'User: ' + parent.name;
+    },
   },
   Query: {
-    users: (): typeof USERS => USERS
+    users: (): typeof USERS => USERS,
   },
   Mutation: {
     createUser: (parent, args): typeof USERS[0] => {
-      const { data } = args
-      console.log('args: ', args)
+      const { data } = args;
+      console.log('args: ', args);
 
       const user = {
         ...data,
-        id: USERS.length + 1
-      }
-      USERS.push(user)
-      return user
-    }
-  }
-}
+        id: USERS.length + 1,
+      };
+      USERS.push(user);
+      return user;
+    },
+  },
+};
 const server = new GraphQLServer({
   typeDefs,
-  resolvers
-})
+  resolvers,
+});
 
-export default server
+export default server;
