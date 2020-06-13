5f9849abcdbde69dd5a04b1e7ca5896b90afc559
Italo Felipe <felipe.italo@hotmail.com>
2020-06-13 12:16:38 -0300

    FIX: Removed unused var, fixed path to customError

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -2,7 +2,7 @@ import { GraphQLServer } from "graphql-yoga";
 import { resolve } from "path";
 import { models as db } from "./models";
 import resolvers from "./resolvers";
-import { logMiddleware, catchErrorsMiddleware } from "./middlewares";
+import { catchErrorsMiddleware } from "./middlewares";
 const typeDefs = resolve(__dirname, "schema.graphql");
 
 const server = new GraphQLServer({
